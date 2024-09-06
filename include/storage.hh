63c7f9dd295be49f77b252682ccad64931870363
sohardforaname <organic_chemistry@foxmail.com>
2024-09-06 21:55:55 +0800

    add storage

--- include/storage.hh
+++ include/storage.hh
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include "miniPage.hh"
+
 #ifndef BF_TREE_STORAGE_HH
 #define BF_TREE_STORAGE_HH
 
@@ -10,28 +12,29 @@ namespace BFTree {
 
 const size_t PAGE_SIZE = 1 << 12;
 
-using Page = unsigned char[PAGE_SIZE];
-
-struct Table {
-  size_t fileNo;
-  size_t fileSize;
-  size_t blockNum;
-  const char* path;
-};
+using Page = unsigned char*;
 
 struct Block {
   size_t fileNo;
   size_t pageNo;
   size_t blockNo;
-  Page* page;
+  Page page;
+};
+
+struct Table {
+  size_t fileNo;
+  size_t fileSize;
+  size_t blockNum;
+  std::vector<Block*> blocks;
 };
 
 extern std::unordered_map<size_t, Table*> fileMap;
-extern std::unordered_map<size_t, Block*> blockMap;
 
 extern Table* readFile(const char* path);
-extern Block* readBlock(const Table* table);
-extern void flushToFile(const Table* table, const Block* block);
+extern Block* readBlock(Table* table, size_t blockId);
+extern int flushToFile(Table* table, const Block* block);
+
+extern int mergeMiniPage(MiniPage* miniPage, Page dataPage);
 
 }  // namespace BFTree
 
--- src/storage.cc
+++ src/storage.cc
@@ -1,14 +1,18 @@
 #include "storage.hh"
 
+#include <fcntl.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
+#include <cstddef>
+#include <cstdio>
 #include <cstring>
 
 namespace BFTree {
 
 Table* readFile(const char* path) {
-  FILE* file = fopen(path, "rb");
-  if (!file) {
+  int fd = open64(path, O_CREAT | O_RDWR);
+  if (fd == -1) {
     return nullptr;
   }
 
@@ -19,15 +23,45 @@ Table* readFile(const char* path) {
   }
 
   Table* table = new Table();
-  table->path = strdup(path);
-  table->fileNo = fileMap.size();
+  table->fileNo = fd;
   table->fileSize = stat_buf.st_size;
   table->blockNum = table->fileSize >> 12;
   fileMap.emplace(table->fileNo, table);
+
+  return table;
 }
 
-Block* readBlock(const Table* table) { return nullptr; }
+Block* readBlock(Table* table, size_t blockId) {
+  size_t readPtr = blockId * (1 << 12);
+  if (lseek64(table->fileNo, readPtr, SEEK_SET) == (off_t)(-1)) {
+    return nullptr;
+  }
 
-void flushToFile(const Table* table, const Block* block) {}
+  Page page = (Page)malloc(PAGE_SIZE);
+  if (read(table->fileNo, page, PAGE_SIZE) == -1) {
+    return nullptr;
+  }
+  Block* block = new Block();
+  block->blockNo = blockId;
+  block->fileNo = table->fileNo;
+  block->pageNo = blockId;
+  block->page = page;
+  table->blocks.push_back(block);
+  return block;
+}
+
+int flushToFile(const Table* table, const Block* block) {
+  size_t writePtr = block->pageNo * (1 << 12);
+  if (lseek64(table->fileNo, writePtr, SEEK_SET) == (off_t)(-1)) {
+    return -1;
+  }
+  int rc = write(table->fileNo, block->page, PAGE_SIZE);
+  return rc;
+}
+
+int mergeMiniPage(MiniPage* miniPage, Page dataPage) {
+  // we assume that kvs in the leaf page is sorted.
+  
+}
 
 }  // namespace BFTree
